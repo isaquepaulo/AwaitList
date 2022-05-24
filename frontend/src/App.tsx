d94bafeee886a91c2b035a7119407182809aac4a
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-05-24 16:57:12 -0300

    Criando projeto e adicionando todas as dependencias

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./assets/styles/custom.scss";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMoon,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return <h1>oi</h1>;
+};
+
+export default Navbar;
